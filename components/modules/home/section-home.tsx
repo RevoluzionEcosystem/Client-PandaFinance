2a74608ec5fac34c95c334e74a8e1fcd5f25818a
Nizar Syahmi bin Nazlan <38692430+nizarsyahmi37@users.noreply.github.com>
2023-11-24 21:03:22 +0700

    Update section-home.tsx

--- components/modules/home/section-home.tsx
+++ components/modules/home/section-home.tsx
@@ -28,7 +28,7 @@ export default function SectionHome() {
                     target="_blank"
                 >
                     <div className="group flex border animate-bounce w-fit mx-auto justify-center bg-muted text-foreground hover:text-secondary-foreground hover:bg-secondary rounded-full shadow-md">
-                        <img className="w-full max-w-[150px] border border-transparent border-r-foreground" src={getCssValue("--background") === "#FAFAFAFF" ? general["pinksale"]["img-light"] : general["pinksale"]["img-dark"]} alt={general["pinksale"].title} />
+                        <img className="w-full max-w-[150px] border border-transparent border-r-foreground" src={getCssValue("--background") === "#fafafa" ? general["pinksale"]["img-light"] : general["pinksale"]["img-dark"]} alt={general["pinksale"].title} />
                         <div className="inline-block text-foreground group:hover:text-secondary-foreground p-1 ps-4">
                             <p className="me-2 inline-block text-sm">
                                 {home.kicker_1}
